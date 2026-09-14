10ebf4a01596ee7b42484c19f35c8fb50cb7fe0a
Vitor-hugo-dev <hugovitor1087@gmail.com>
2026-09-14 16:25:09 -0300

    Add primitive types demonstration in Python

---
+++ 01-tipos-primitivos/tipos-em-python/tipos.py
@@ -0,0 +1,18 @@
+# Tipos primitivos em Python
+
+numero_inteiro = 10
+numero_decimal = 10.5
+texto = "Olá, mundo!"
+verdadeiro = True
+
+print("Inteiro:", numero_inteiro)
+print("Decimal:", numero_decimal)
+print("Texto:", texto)
+print("Booleano:", verdadeiro)
+
+print("\nTipos:")
+
+print(type(numero_inteiro))
+print(type(numero_decimal))
+print(type(texto))
+print(type(verdadeiro))
